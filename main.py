6547565ec54a9dd7b43f6109b14c1fbbc740ac0d
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-06-24 18:35:41 -0400

    implement random search run mode

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bcorag import misc_functions as misc_fns
 from bcorag import option_picker as op
 from bcorag.bcorag import BcoRag
 from parameter_search.grid_search import BcoGridSearch
+from parameter_search.random_search import BcoRandomSearch
 from bcorag.custom_types import (
     GitFilter,
     GitFilters,
@@ -28,6 +29,56 @@ def main() -> None:
     )
     options = parser.parse_args()
     run_mode = options.run_mode.lower().strip()
+
+    filenames = ["./bcorag/test_papers/High resolution measurement.pdf"]
+    loaders = "SimpleDirectoryReader"
+    chunking_config = [
+        "1024 chunk size/20 chunk overlap",
+        "2048 chunk size/50 chunk overlap",
+    ]
+    embedding_model = "text-embedding-3-large"
+    vector_store = "VectorStoreIndex"
+    similarity_top_k = [2, 3, 4]
+    llms = ["gpt-3.5-turbo", "gpt-4-turbo"]
+
+    github_url = "https://github.com/dpastling/plethora"
+    git_info = misc_fns.extract_repo_data(github_url)
+    if git_info is None:
+        misc_fns.graceful_exit(1, "Error parsing github URL.")
+
+    git_filters: list[GitFilters] = []
+    directory_filter = create_git_filters(
+        filter_type=GithubRepositoryReader.FilterType.EXCLUDE,
+        filter=GitFilter.DIRECTORY,
+        value=["logs", "fastq", "data"],
+    )
+    git_filters.append(directory_filter)
+
+    file_ext_filter = create_git_filters(
+        filter_type=GithubRepositoryReader.FilterType.EXCLUDE,
+        filter=GitFilter.FILE_EXTENSION,
+        value=["txt", "gz", "bed"],
+    )
+    git_filters.append(file_ext_filter)
+
+    git_data = create_git_data(
+        user=git_info[0], repo=git_info[1], branch="master", filters=git_filters
+    )
+
+    git_file_data = create_git_data_file_config(
+        os.path.basename(filenames[0]), git_data
+    )
+
+    search_space = init_search_space(
+        filenames=filenames,
+        loader=loaders,
+        chunking_config=chunking_config,
+        embedding_model=embedding_model,
+        vector_store=vector_store,
+        similarity_top_k=similarity_top_k,
+        llm=llms,
+        git_data=[git_file_data],
+    )
 
     match run_mode:
 
@@ -57,62 +108,22 @@ def main() -> None:
                 "################################## RUN START ##################################"
             )
 
-            filenames = ["./bcorag/test_papers/High resolution measurement.pdf"]
-            loaders = "SimpleDirectoryReader"
-            chunking_config = [
-                "1024 chunk size/20 chunk overlap",
-                "2048 chunk size/50 chunk overlap",
-            ]
-            embedding_model = "text-embedding-3-large"
-            vector_store = "VectorStoreIndex"
-            similarity_top_k = [2, 3, 4]
-            llms = ["gpt-3.5-turbo", "gpt-4-turbo"]
-
-            github_url = "https://github.com/dpastling/plethora"
-            git_info = misc_fns.extract_repo_data(github_url)
-            if git_info is None:
-                misc_fns.graceful_exit(1, "Error parsing github URL.")
-
-            git_filters: list[GitFilters] = []
-            directory_filter = create_git_filters(
-                filter_type=GithubRepositoryReader.FilterType.EXCLUDE,
-                filter=GitFilter.DIRECTORY,
-                value=["logs", "fastq", "data"],
-            )
-            git_filters.append(directory_filter)
-
-            file_ext_filter = create_git_filters(
-                filter_type=GithubRepositoryReader.FilterType.EXCLUDE,
-                filter=GitFilter.FILE_EXTENSION,
-                value=["txt", "gz", "bed"],
-            )
-            git_filters.append(file_ext_filter)
-
-            git_data = create_git_data(user=git_info[0], repo=git_info[1], branch="master", filters=git_filters)
-
-            git_file_data = create_git_data_file_config(
-                os.path.basename(filenames[0]), git_data
-            )
-
-            search_space = init_search_space(
-                filenames=filenames,
-                loader=loaders,
-                chunking_config=chunking_config,
-                embedding_model=embedding_model,
-                vector_store=vector_store,
-                similarity_top_k=similarity_top_k,
-                llm=llms,
-                git_data=[git_file_data],
-            )
-
             grid_search = BcoGridSearch(search_space)
             grid_search.train()
 
             misc_fns.graceful_exit()
 
         case "random-search":
-            # TODO : implement
-            pass
+
+            logger = misc_fns.setup_root_logger("./logs/random-search.log")
+            logger.info(
+                "################################## RUN START ##################################"
+            )
+
+            random_search = BcoRandomSearch(search_space, subset_size=5)
+            random_search.train()
+
+            misc_fns.graceful_exit()
 
         case _:
 
