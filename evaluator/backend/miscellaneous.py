a65379bbc0fad3786f53395c3687d8bae0d88277
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-07-01 12:36:55 -0400

    more robust state logging

--- evaluator/backend/miscellaneous.py
+++ evaluator/backend/miscellaneous.py
@@ -1,7 +1,8 @@
 from bcorag import misc_functions
-from .custom_types import AppAttributes, AppState, StateKey
+from .custom_types import AppAttributes, AppState, AppStateKey, RunState, RunStateKey
 import customtkinter as ctk  # type: ignore
-from typing import NoReturn, get_args
+from typing import NoReturn, get_args, Literal, cast
+import logging
 
 
 def exit_app(app_state: AppState | AppAttributes) -> NoReturn:
@@ -10,25 +11,46 @@ def exit_app(app_state: AppState | AppAttributes) -> NoReturn:
     Parameters
     ----------
     app_state : AppState or AppAttributes
-        The app state or attributes (if state hasn't been initialized 
-        yet) containing output data to write to disk before exiting 
+        The app state or attributes (if state hasn't been initialized
+        yet) containing output data to write to disk before exiting
         (prevents lost save data).
     """
     # TODO : implement attributes writes
     misc_functions.graceful_exit(0)
 
 
-def log_state(app_state: AppState) -> None:
+def log_state(state: AppState | RunState, state_type: Literal["app", "run"]) -> None:
     """Logs the app state.
 
     Parameters
     ----------
-    app_state : AppState
-        The app state.
+    state : AppState or RunState
+        The state to log.
+    state_type : "app" or "run"
+        The type of state being logged.
     """
-    log_str = "App state:\n"
-    key: StateKey
-    for key in get_args(StateKey):
-        if key == "logger":
-            continue
-        log_str += f"\t{key}: {app_state[key]}\n"
+    app_state_flag = True if state_type.lower().strip() == "app" else False
+    log_str = "App state:\n" if app_state_flag else "Run state:\n"
+
+    if app_state_flag:
+        app_state = cast(AppState, state)
+        app_key: AppStateKey
+        for app_key in get_args(AppStateKey):
+            if app_key == "logger":
+                continue
+            log_str += f"\t{app_key}: {app_state[app_key]}\n"
+    else:
+        run_state = cast(RunState, state)
+        run_key: RunStateKey
+        for run_key in get_args(RunStateKey):
+            if run_key in {
+                "generated_domain",
+                "human_curated_domain",
+                "reference_nodes",
+                "param_set",
+                "logger",
+            }:
+                continue
+            log_str += f"\t{run_key}: {run_state[run_key]}\n"
+
+    state["logger"].info(log_str)
