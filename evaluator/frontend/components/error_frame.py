2797fde9a84717207e5ece360b430da5f4e7bee7
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-07-02 17:13:30 -0400

    finish error frame

--- evaluator/frontend/components/error_frame.py
+++ evaluator/frontend/components/error_frame.py
@@ -1,5 +1,14 @@
 import customtkinter as ctk  # type: ignore
-from evaluator.backend.custom_types import AppState, RunState
+from evaluator.backend.custom_types import (
+    AppState,
+    ErrorEval,
+    RunState,
+    create_error_val,
+    reverse_cast_error_type,
+)
+from evaluator.backend import DEFAULT_SCORES
+
+EVAL_DEFAULTS = DEFAULT_SCORES["error_eval"]
 
 
 class ErrorFrame(ctk.CTkFrame):
@@ -12,6 +21,7 @@ class ErrorFrame(ctk.CTkFrame):
         super().__init__(master, **kwargs)
         self.state = app_state
         self.run = run_state
+        self.error_eval = self.run["eval_data"]["error_eval"]
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -23,7 +33,9 @@ class ErrorFrame(ctk.CTkFrame):
             row=0, column=0, padx=self.state["padding"], pady=self.state["padding"]
         )
 
-        self.inf_err_var = ctk.StringVar(value="off")
+        self.inf_err_var = ctk.StringVar(
+            value=reverse_cast_error_type(self.error_eval["inferred_knowledge_error"])
+        )
         self.inf_checkbox = ctk.CTkCheckBox(
             master=self,
             text="Inferred Knowledge Error",
@@ -35,11 +47,13 @@ class ErrorFrame(ctk.CTkFrame):
             row=1,
             column=0,
             padx=self.state["padding"],
-            pady=self.state["padding"],
+            pady=self.state["padding"] // 4,
             sticky="w",
         )
 
-        self.ext_err_var = ctk.StringVar(value="off")
+        self.ext_err_var = ctk.StringVar(
+            value=reverse_cast_error_type(self.error_eval["external_knowledge_error"])
+        )
         self.ext_checkbox = ctk.CTkCheckBox(
             master=self,
             text="External Knowledge Error",
@@ -51,11 +65,94 @@ class ErrorFrame(ctk.CTkFrame):
             row=2,
             column=0,
             padx=self.state["padding"],
-            pady=self.state["padding"],
+            pady=self.state["padding"] // 4,
             sticky="w",
+        )
+
+        self.other_err_var = ctk.StringVar(
+            value=reverse_cast_error_type(self.error_eval["other_error"])
+        )
+        self.other_err_checkbox = ctk.CTkCheckBox(
+            master=self,
+            text="Other Error",
+            variable=self.other_err_var,
+            onvalue="on",
+            offvalue="off",
+        )
+        self.other_err_checkbox.grid(
+            row=3,
+            column=0,
+            padx=self.state["padding"],
+            pady=self.state["padding"] // 4,
+            sticky="w",
+        )
+
+        self.error_notes_label = ctk.CTkLabel(
+            master=self, text="Notes", font=(self.state["font"], 16, "bold")
+        )
+        self.error_notes_label.grid(
+            row=4,
+            column=0,
+            padx=self.state["padding"],
+            pady=self.state["padding"] // 2,
+            sticky="w"
+        )
+
+        self.error_notes = ctk.CTkTextbox(
+            master=self, wrap="word", font=(self.state["font"], 18)
+        )
+        self.error_notes.grid(
+            row=5,
+            column=0,
+            padx=self.state["padding"] // 2,
+            pady=self.state["padding"] // 2,
+            sticky="nsew",
         )
 
     def update_state(self, app_state: AppState, run_state: RunState) -> None:
         """Update the state."""
         self.run = run_state
-        self.state=app_state
+        self.state = app_state
+        self.error_eval = self.run["eval_data"]["error_eval"]
+
+        self.inf_err_var = ctk.StringVar(
+            value=reverse_cast_error_type(
+                self.error_eval.get(
+                    "inferred_knowledge_error",
+                    EVAL_DEFAULTS["inferred_knowledge_error"],
+                )
+            )
+        )
+        self.inf_checkbox.configure(variable=self.inf_err_var)
+
+        self.ext_err_var = ctk.StringVar(
+            value=reverse_cast_error_type(
+                self.error_eval.get(
+                    "external_knowledge_error",
+                    EVAL_DEFAULTS["external_knowledge_error"],
+                )
+            )
+        )
+        self.ext_checkbox.configure(variable=self.ext_err_var)
+
+        self.other_err_var = ctk.StringVar(
+            value=reverse_cast_error_type(
+                self.error_eval.get("other_error", EVAL_DEFAULTS["other_error"])
+            )
+        )
+        self.other_err_checkbox.configure(variable=self.other_err_var)
+
+        self.error_notes.delete(0.0, "end")
+        self.error_notes.insert(
+            0.0, self.error_eval.get("notes", EVAL_DEFAULTS["notes"])
+        )
+
+    def get_results(self) -> ErrorEval:
+        """Returns the error evaluations."""
+        error_eval = create_error_val(
+            inf_err=self.inf_err_var.get(),
+            ext_err=self.ext_err_var.get(),
+            other_err=self.other_err_var.get(),
+            notes=self.error_notes.get(0.0, "end").strip(),
+        )
+        return error_eval
