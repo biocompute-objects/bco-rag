af38a0227ff141cd71d1115850908a49d6ad3e54
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-08-20 17:15:42 -0400

    init

---
+++ aggregator/aggregator.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+
+import subprocess
+import platform
+
+class Aggregator:
+    """Classs to handle the in progress data aggregator.
+
+    Attributes
+    ----------
+    """
+
+    def __init__(self, include: str, exclude: str):
+        # TODO
+        pass
