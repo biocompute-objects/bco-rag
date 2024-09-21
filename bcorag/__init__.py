b2ec39552d0984f04c27452466d1d06cb09ba687
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-09-20 22:43:28 -0400

    Update to v0.3.2

--- bcorag/__init__.py
+++ bcorag/__init__.py
@@ -3,9 +3,8 @@
 The BcoRag module defines the core functionality for the BioCompute Object RAG Assistant.
 """
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "seankim658"
 
 TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S %Z%z"
 TIMEZONE = "US/Eastern"
-EVALUATION_LLM = "gpt-4"
